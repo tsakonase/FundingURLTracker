c80fdab2c61151da872f64c742de353585634e93
Tsakonas, Efthymios (ELS-AMS) <e.tsakonas@elsevier.com>
2021-02-28 19:33:12 +0200

    Code refactoring - the output of crawler goes into BS4 and subsequently Keyword Checking

---
+++ tutorial/Utils/Tokenizers.py
@@ -0,0 +1,38 @@
+import nltk
+import unidecode
+from nltk.tokenize import word_tokenize, sent_tokenize
+from string import punctuation
+from nltk.corpus import stopwords
+
+nltk.download("stopwords")
+nltk.download("wordnet")
+
+class WordTokenizeText:
+
+    def __init__(self):
+        self.stopword_set = set(stopwords.words("english"))
+        self.word_stemmer = nltk.WordNetLemmatizer()
+        self.punctuation = set(punctuation)
+
+    def purge_punct_tok(self, word):
+        return ''.join(c for c in word if c not in punctuation)
+
+    def word_tokenize(self, text, remove_punct = True, remove_stopwords = False, stem_words = False, purge_token_punct=False):
+        # unidecode the text:
+        n_text = unidecode.unidecode(text).replace("\n", " ")
+        # nltk tokenization
+        tokens = [word.lower() for sent in sent_tokenize(n_text) for word in word_tokenize(sent)]
+        # if remove punctuation:
+        if remove_punct:
+            tokens = [token for token in tokens if not all(char in self.punctuation for char in token)]
+        # if remove stopwords:
+        if remove_stopwords:
+            tokens = [token for token in tokens if token not in self.stopword_set]
+        # if stem words:
+        if stem_words:
+            tokens = [self.word_stemmer.lemmatize(token) for token in tokens]
+        # if purge punctuation within a token:
+        if purge_token_punct:
+            tokens = [self.purge_punct_tok(token) for token in tokens]
+
+        return tokens
--- tutorial/tutorial/spiders/myspider.py
+++ tutorial/tutorial/spiders/myspider.py
@@ -1,82 +1,37 @@
 import scrapy
-import pandas as pd
-from string import punctuation
+from bs4 import BeautifulSoup
+from Utils.SnippetExtractor import KeywordChecker
 
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+class TermsOfUseSpider(scrapy.Spider):
+    name = "tou_spider"
 
     def start_requests(self):
-        data = pd.read_excel("/Users/tsakonase/tutorial/tutorial/spiders/Training - Data G&F - Only URLs - SS and MM.xlsx")
-        toU_URL_col = data[data['ToU (Terms of Use should refer to copyright/reuse of data) URL'].notnull()]
-
-        urls = list(toU_URL_col['ToU (Terms of Use should refer to copyright/reuse of data) URL'])
-
-        for url in urls[:2]:
+        # provide a list of URLs
+        for url in [
+            "https://www.alexslemonade.org/terms-of-use"
+        ]:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse (self, response):
-        page = ''.join(c for c in ''.join(response.url.split("/")) if c != ' ' and c not in punctuation)
-        filename = f'/Users/tsakonase/tutorial/tutorial/spiders/Results/ToU-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+        # try to parse the response text via BS:
+        soup = None
+        try:
+            soup = BeautifulSoup(response.body, "html.parser")
+        except:
+            print ("BeautifulSoup failed to parse the response, exiting...")
+            exit()
 
+        # try to extract text from soup
+        try:
+            txt = soup.find_all(text=True)
+            text = ' '.join([s for s in list(txt) if s.strip().strip("\n").strip()])
+        except:
+            print ("Failed to extract the text from HTML, exiting...")
+            exit()
 
-# # # GEOGRAFICAL ENTITY: Q27096213
-# # Q8502 --> mountain
-# # Q813672 --> basin
-# # Q820477 --> mine
-# # Q47089 --> fault (geology)
-#
-#
-# from SPARQLWrapper import SPARQLWrapper, JSON
-#
-# sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
-#
-# QUERY = """
-# SELECT DISTINCT ?item ?name ?coord ?lat ?lon ?globe
-# {
-#    ?item wdt:P31 wd:Q820477 ;
-#          p:P625 [
-#            psv:P625 [
-#              wikibase:geoLatitude ?lat ;
-#              wikibase:geoLongitude ?lon ;
-#              wikibase:geoGlobe ?globe ;
-#            ] ;
-#            ps:P625 ?coord
-#          ]
-#   FILTER ( ?globe = wd:Q2 )
-#   SERVICE wikibase:label {
-#     bd:serviceParam wikibase:language "en" .
-#     ?item rdfs:label ?name
-#    }
-# }
-# LIMIT 100000
-# """
-#
-# QUERY2 = """
-#
-# SELECT ?s ?desc WHERE {
-#
-# ?s wdt:P279+ wd:Q27096213 .
-#
-# OPTIONAL {
-#
-# ?s rdfs:label ?desc filter (lang(?desc) = "en").
-#
-# }
-#
-# }
-#
-# """
-#
-# sparql.setQuery(QUERY2)
-# sparql.setReturnFormat(JSON)
-#
-# results = sparql.query().convert()
-#
-# import json
-# with open("./file.txt", "w") as json_file:
-#     for res in results['results']['bindings']:
-#         json_file.write(json.dumps(res))
+        # extract snippets:
+        snippets = KeywordChecker().extract_snippets(text)
+
+        # just log for now, until we know more:
+        self.log("\n".join(snippets))
 
